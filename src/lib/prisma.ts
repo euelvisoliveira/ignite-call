0540c57ba3d0a2fdac989295b333d5781133b844
Elvis_Ribeiro Computer_home <elvis.ribeiro.deoliveira@hotmail.com>
2023-04-24 21:17:28 -0300

    aplicando e configurando o Prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient({
+  log: ['query'], // essa opção de log, vai fazer automaticamente o log de todos sql executados no banco de dados
+})
